17cb007fa7fe05f053257317e069cc3b929e34b7
Fédi <fedielmanoubi@gmail.com>
2023-09-14 17:11:51 +0200

    video2 2:48min

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use fr;
+use Faker\Factory;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker=Factory::create("fr_FR");
+        
+        $manager->flush();
+    }
+}
