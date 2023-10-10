6badd33df27334cb0b2dc895b374ec5c20367199
Fédi <123569039+Fedi78@users.noreply.github.com>
2023-10-10 22:59:17 +0200

    ajout message flash

--- src/Controller/Admin/ArtisteController.php
+++ src/Controller/Admin/ArtisteController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Artiste;
 use App\Form\ArtisteType;
 use App\Repository\ArtisteRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
@@ -31,10 +32,19 @@ class ArtisteController extends AbstractController
      /**
      * @Route("/admin/artiste/ajout", name="admin_artiste_ajout", methods={"GET","POST"})
      */
-    public function ajoutArtiste()
+    public function ajoutArtiste(Request $request, EntityManagerInterface $manager)
     {
+
             $artiste=new Artiste();
             $form=$this->createForm(ArtisteType::class, $artiste);
+            $form->handleRequest($request);
+            if($form->isSubmitted() && $form->isValid())
+            {
+                $manager->persist($artiste);
+                $manager->flush();
+                $this->addFlash("success","L'artiste a bien été Ajouté");
+                return $this->redirectToRoute('admin_artistes');
+            }
             return $this->render('admin/artiste/formAjoutArtiste.html.twig', [
             'formArtiste' => $form->createView()
         ]);
--- src/Form/ArtisteType.php
+++ src/Form/ArtisteType.php
@@ -6,18 +6,36 @@ use App\Entity\Artiste;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\UrlType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class ArtisteType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('nom')
-            ->add('description')
-            ->add('site')
-            ->add('image')
-            ->add('type')
+            ->add('nom', TextType::class,[
+                'label'=>"Nom de L'artiste",
+                'attr'=>[
+                    "placeholder"=>"Saisir le nom de L'artiste"
+                ]
+            ])
+            ->add('description', TextareaType::class)
+            ->add('site', UrlType::class)
+            ->add('image',TextType::class)
+            ->add('type', ChoiceType::class, [
+
+                "choices"=>[
+                    "solo"=>0,
+                    "groupe"=>1
+                ]
+            ])
+                
+                    
+            
            // ->add('valider', SubmitType::class)
         ;
     }
